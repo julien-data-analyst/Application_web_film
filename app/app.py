4256500cbd0acf50cad63f55a4500346888ee9e9
julien-data-analyst-V2 <julienrenoult76460@gmail.com>
2024-12-03 15:00:01 +0100

    Initialisation du projet et de sa structure

---
+++ app/app.py
@@ -0,0 +1,6 @@
+#################################################
+# Auteurs : Julien RENOULT - Yop JUGUL DALYOP - Gabriel DURAND - Ryan DOBIGNY
+# Date : 03/12/2024
+# Sujet : création d'une application web (lanceur)
+#################################################
+
