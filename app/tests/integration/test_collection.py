ec55e67df8cbb651da9592c714bc923aa6126aae
John Doe <yopdalyop@gmail.com>
2025-02-28 16:01:18 +0100

    continued collection tests.

--- app/tests/integration/test_collection.py
+++ app/tests/integration/test_collection.py
@@ -1,1 +1,8 @@
+from flask.testing import FlaskClient
 from modele_bdd import Collection
+
+def test_relation_collection_film(client: FlaskClient):
+    pass
+
+def test_collection_json_avec_films(client: FlaskClient):
+    pass
