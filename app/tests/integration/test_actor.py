1d66bcf76c8a2d8d045d066be5b8fbab4a830b2a
julien-data-analyst-V2 <julienrenoult76460@gmail.com>
2025-03-19 21:22:08 +0100

    Ajout de la couverture de tests HTML

--- app/tests/integration/test_actor.py
+++ app/tests/integration/test_actor.py
@@ -2,6 +2,7 @@ import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from modele_bdd import db, Film, Acteur, film_acteurs
+import datetime 
 
 @pytest.fixture
 def test_db():
@@ -13,21 +14,26 @@ def test_db():
 
     yield session  
     session.close() 
+
 def test_film_actor_relationship(test_db):
     """Vérifie que la relation entre un film et un acteur est bien enregistrée."""
     
     # Création d'un film
-    film = Film(id=1, title="Inception", release_date="2010-07-16")
+    film = Film(id=1, title="Inception", 
+                release_date=datetime.datetime(2010, 7, 16),
+                vote_count=2000)
     test_db.add(film)
     
     # Création d'un acteur
     actor = Acteur(id=1, nom="DiCaprio", prenom="Leonardo")
     test_db.add(actor)
     
-    # Liaison du film et de l'acteur dans la table d'association
-    test_db.execute(film_acteurs.insert().values(id_film=film.id, id_acteur=actor.id))
-    
     # Commit des changements
+    test_db.commit()
+
+    # Liaison du film et de l'acteur dans la table d'association
+    film.acteurs.append(actor)
+
     test_db.commit()
 
     # Vérification que l'association existe
